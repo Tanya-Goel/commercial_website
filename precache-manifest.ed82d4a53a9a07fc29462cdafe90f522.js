dba9a3d55b14f9b02d14f7e56a709e724b5085f3
Tanya-Goel <goeltanya01@gmail.com>
2020-07-27 13:49:45 +0530

    Updates

---
+++ precache-manifest.ed82d4a53a9a07fc29462cdafe90f522.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e6f01ef313b3062b0cc68b01e53f55c",
+    "url": "/commercial_website/index.html"
+  },
+  {
+    "revision": "3278d48ebead54cfd8df",
+    "url": "/commercial_website/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "7dfb1979828b249c51cf",
+    "url": "/commercial_website/static/css/main.b0cd35b8.chunk.css"
+  },
+  {
+    "revision": "3278d48ebead54cfd8df",
+    "url": "/commercial_website/static/js/2.20d470ff.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/commercial_website/static/js/2.20d470ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7dfb1979828b249c51cf",
+    "url": "/commercial_website/static/js/main.ad798004.chunk.js"
+  },
+  {
+    "revision": "848f74018660a1c2d6e6",
+    "url": "/commercial_website/static/js/runtime-main.6b563a26.js"
+  },
+  {
+    "revision": "bdbbe54f99c37fdc6a525b3ccfbe220e",
+    "url": "/commercial_website/static/media/analyst.bdbbe54f.jpg"
+  },
+  {
+    "revision": "b454fdb297de369ffa52b5aaba8cf4af",
+    "url": "/commercial_website/static/media/android.b454fdb2.jpg"
+  },
+  {
+    "revision": "adef3842f5dff879988bd453d426dede",
+    "url": "/commercial_website/static/media/hacking.adef3842.jpg"
+  },
+  {
+    "revision": "7626033c54451151cd7bab449c5c1e1f",
+    "url": "/commercial_website/static/media/l.7626033c.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/commercial_website/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "97c1df7295f0163561977b1730366188",
+    "url": "/commercial_website/static/media/weddev.97c1df72.jpeg"
+  },
+  {
+    "revision": "761f98e782805ae54408f9aeee09facb",
+    "url": "/commercial_website/static/media/who-we-are.761f98e7.jpg"
+  }
+]);
